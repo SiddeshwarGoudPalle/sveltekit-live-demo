9e88b853013ecfadcc69d73bf14184547a658963
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-07-09 17:54:05 +0530

    added pl-20

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
-<p class="hover:underline font-bold hover:text-red-500 pt-20 ml-20">
+<p class=" hover:underline font-bold hover:text-red-500 pt-20 pl-20 ml-20">
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>

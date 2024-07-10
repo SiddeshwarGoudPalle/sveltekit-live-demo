173a0eb08cb30320b5c58ddbd0d770381b598074
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-07-10 16:49:31 +0530

    Done with the code

--- src/routes/blog/[slug]/+page.server.js
+++
@@ -1,7 +0,0 @@
-import { sql } from "@vercel/postgres";
-
-export async function load({ params, locals }) {
-  return {
-    names: await sql`SELECT * from Names where user_id='${locals.user}'`
-  }
-}
--- src/routes/blog/list/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  import Table from "$lib/Table.svelte";
-</script>
-
-<h1>List of blogs</h1>
-<Table />

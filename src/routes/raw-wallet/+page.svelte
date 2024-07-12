ed6ea6b3290ea97e47b6e922d1cac7f63ff3a84d
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-07-12 17:39:10 +0530

    Did Nothing

--- src/routes/raw-wallet/+page.svelte
+++ src/routes/raw-wallet/+page.svelte
@@ -7,19 +7,19 @@
   function createKeys() {
     const privKey = PrivateKey.fromRandom();
     WIF = privKey.toWif();
-    console.log("Private Key WIF format is ", WIF);
+    console.log("Private Key WIF format is: ", WIF);
     address = privKey.toAddress();
-    console.log("Your bitcoin address is ", address);
+    console.log("Your bitcoin address is: ", address);
   }
 </script>
 
-<section>
+<section class="m-20">
   <button
     class=" bg-black text-white p-2 rounded mx-auto"
     on:click={createKeys}
   >
     Generate New Wallet Keys
   </button>
-  <p>Private Key WIF format is {WIF}</p>
-  <p>Your BitCoin Address is {address}</p>
+  <p class="pt-10">Private Key WIF format is : {WIF}</p>
+  <p class="pt-10">Your BitCoin Address is : {address}</p>
 </section>
